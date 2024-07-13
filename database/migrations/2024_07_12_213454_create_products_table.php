91f843bd9af24e6489ac72c3d4a57c743f4a8543
mor <tallpape32@gmail.com>
2024-07-13 02:14:02 +0000

    migration table products

---
+++ database/migrations/2024_07_12_213454_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->integer('prix');
+            $table->integer('stock')->default(1)->comment('1=En stock, 0=fini');
+            $table->text('description')->nullable();
+            $table->text('info');
+            $table->text('image');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
