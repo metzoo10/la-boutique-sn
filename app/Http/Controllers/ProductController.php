0020733dc00045d971e44a12e6c302ea19b6efac
mor <tallpape32@gmail.com>
2024-07-13 02:31:51 +0000

    modification model User et Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,54 +6,55 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
-{   
-    // Fonction "show" avec paramètre $id qui récupère dans un tableau compact, les produits par leurs id.
+{ 
+// Fonction "show" avec paramètre $id qui récupère dans un tableau compact, les produits par leurs id.
 
-    public function show($id)
-    {
-        $produit = Product::find($id);
+public function show($id)
+{
+$produit = Product::find($id);
 
-        return view('detail', compact('produit'));
-    }
-
-    // Fonction "index" avec la méthode Request qui récupère tous les produits 
-
-    public function index(Request $request)
-    {
-        // utilise les conditions switch pour faire des tris par catégorie, nom et prix.
-        $query = Product::query();
-
-        if ($request->has('sort')) {
-            $sortOption = $request->input('sort');
-            switch ($sortOption) {
-                case 'categorie_asc':
-                    $query->orderBy('categorie', 'asc');
-                    break;
-                case 'categorie_desc':
-                    $query->orderBy('categorie', 'desc');
-                    break;
-                case 'prix_asc':
-                    $query->orderBy('prix', 'asc');
-                    break;
-                case 'prix_desc':
-                    $query->orderBy('prix', 'desc');
-                    break;
-                case 'nom_asc':
-                    $query->orderBy('nom', 'asc');
-                    break;
-                case 'nom_desc':
-                    $query->orderBy('nom', 'desc');
-                    break;
-            }
-        }
-
-        // enregistrer la requête dans $produits puis crée une pagination de 12 produits par page avec "paginate".
-        $produits = $query->paginate(12);
-
-        // récupère les catégories de manière distincte.
-        $categories = Product::select('categorie')->distinct()->get();
-
-        // envoie un tableau des résultats de requête à la vue "Boutique".
-        return view('boutique', compact('produits' ,'categories'));
-    }
+return view('detail', compact('produit'));
 }
+
+// Fonction "index" avec la méthode Request qui récupère tous les produits 
+
+public function index(Request $request)
+{
+// utilise les conditions switch pour faire des tris par catégorie, nom et prix.
+$query = Product::query();
+
+if ($request->has('sort')) {
+$sortOption = $request->input('sort');
+switch ($sortOption) {
+case 'categorie_asc':
+$query->orderBy('categorie', 'asc');
+break;
+case 'categorie_desc':
+$query->orderBy('categorie', 'desc');
+break;
+case 'prix_asc':
+$query->orderBy('prix', 'asc');
+break;
+case 'prix_desc':
+$query->orderBy('prix', 'desc');
+break;
+case 'nom_asc':
+$query->orderBy('nom', 'asc');
+break;
+case 'nom_desc':
+$query->orderBy('nom', 'desc');
+break;
+}
+}
+
+// enregistrer la requête dans $produits puis crée une pagination de 12 produits par page avec "paginate".
+$produits = $query->paginate(12);
+
+// récupère les catégories de manière distincte.
+$categories = Product::select('categorie')->distinct()->get();
+
+// envoie un tableau des résultats de requête à la vue "Boutique".
+return view('boutique', compact('produits' ,'categories'));
+}
+}
+
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,8 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $table = 'produits';
-    protected $primaryKey = 'id';
-    protected $fillable = ['nom', 'prix', 'stock', 'description', 'info', 'image', 'categorie'];
+    protected $table = 'products';
 
+    protected $fillable = ['nom', 'prix', 'stock', 'description', 'info', 'image', 'category_id'];
+
+
+     // Définir la relation "many-to-one" avec le modèle Category
+     public function category()
+     {
+         return $this->belongsTo(Category::class);
+     }
 }
