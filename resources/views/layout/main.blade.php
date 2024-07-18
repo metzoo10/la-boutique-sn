a55804d6717d257ce68fb56542d1295d7179d954
mor <tallpape32@gmail.com>
2024-07-18 15:01:16 +0000

    UPDATE

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>@yield('title')</title>
+	
 	<link rel="stylesheet" href="/css/styles.css">
 	<link rel="stylesheet" href="/css/navbar.css">
 	<link rel="stylesheet" href="/css/footer.css">
