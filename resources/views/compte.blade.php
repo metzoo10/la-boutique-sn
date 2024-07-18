5ff09cc66842ca74dc1a64dd654dc2c9d6a476a7
mor <tallpape32@gmail.com>
2024-07-18 14:54:49 +0000

    Afficher les informations du client dans son compte

--- resources/views/compte.blade.php
+++ resources/views/compte.blade.php
@@ -13,7 +13,50 @@
 		</nav>
 		
 		
+		
+		
 	</div>
+	<div class="container mt-5 mb-5 border border-secondary py-4">
+	@auth
+
+	
+		<div class="h5 pb-2 mt-2 mb-4 text-uppercase text-muted border-bottom border-secondary">Information personnel</div>
+		<form class="row g-3">
+		  <div class="col-md-6">
+		    <label for="inputNom4" class="form-label fw-light">Nom & Prenom<span class="text-info"></span></label>
+		    <input type="text" class="form-control" id="inputNom4" readonly value="{{Auth::user()->name}}">
+		  </div>
+		  <div class="col-md-6">
+		    <label for="inputPrenom4" class="form-label fw-light">Email<span class="text-info"></span></label>
+		    <input type="text" class="form-control" id="inputPrenom4"readonly value="{{Auth::user()->email}}">
+		  </div>
+		  <div class="col-6">
+		    <label for="inputAddress" class="form-label fw-light">Adresse<span class="text-info"></span></label>
+		    <input type="text" class="form-control" id="inputAddress"readonly value="{{Auth::user()->adresse}}">
+		  </div>
+		  <div class="col-6">
+		    <label for="inputPhone2" class="form-label fw-light">Téléphone<span class="text-info"></span></label>
+		    <input type="number" class="form-control" id="inputPhone2" value="{{Auth::user()->telephone}}" >
+		  </div>
+		  
+		
+
+		
+		</form>
+		</div>
+	
+		
+	@endauth
+	@guest
+	<div class="alert alert-warning text-center alert-dismissible fade show" role="alert">
+		<h1>Aucune information, svp connectez-vous</h1>
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+	</div>
+	@endguest
+</div>
+	
+	
+
 
 	
 	
