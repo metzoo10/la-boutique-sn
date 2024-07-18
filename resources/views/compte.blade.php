88b811897e274433160318d4e9d2fa0e02f993c7
mor <tallpape32@gmail.com>
2024-07-18 02:15:20 +0000

    AFFICHAGE DU NOM DU CLIENT CONNECTER SUR LE BREADCRUMB

--- resources/views/compte.blade.php
+++ resources/views/compte.blade.php
@@ -8,19 +8,13 @@
 	  		<ol class="breadcrumb">
 	    		<li class="breadcrumb-item"><a class="text-uppercase text-info text-decoration-none" href="/">Accueil</a></li>
 	    		<li class="breadcrumb-item active text-uppercase" aria-current="page">Compte</li>
+				@auth <li class="breadcrumb-item active text-uppercase" aria-current="page">{{Auth::user()->name}}</li>@endauth
 	  		</ol>
 		</nav>
+		
+		
 	</div>
 
-	 @auth <h1>Bonjour {{Auth::user()->name}} </h1>
-
-
-
-	 
-	 	@guest
-			
-		@endguest
-	    
-	 @endauth
+	
 	
 @endsection
