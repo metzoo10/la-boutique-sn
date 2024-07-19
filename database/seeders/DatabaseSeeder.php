1442a399cc01a17f4a6a1626d092e9de800aec00
metzoo10 <mister_mef365@outlook.com>
2024-07-19 12:36:43 +0000

    DatabaseTableSeeder modifié

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        $this->call(CategoriesTableSeeder::class);
+
+        $this->call(ProduitsTableSeeder::class);
+        /*
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
     }
 }
