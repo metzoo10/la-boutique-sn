35cc70c2d8436428663bf41d1984ff586daad9d3
metzoo10 <mister_mef365@outlook.com>
2024-07-23 18:52:58 +0000

    Page à propos modifié : photos membres équipe

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -82,19 +82,19 @@
 		      <p>Email : <a href="mailto:mamediarra.mane@unchk.edu.sn">mamediarra.mane@unchk.edu.sn</a></p>
 		    </div>
 		    <div class="carousel-item">
-		      <img src="images/membres/3.jpg" class="img-thumbnail" alt="..." width="250px">
+		      <img src="images/membres/8.jpg" class="img-thumbnail" alt="..." width="250px">
 		      <h5 class="mt-5">Mor Tall</h5>
 		      <p>Étudiant</p>
 		      <p>Email : <a href="mailto:mor.tall@unchk.edu.sn">mor.tall@unchk.edu.sn</a></p>
 		    </div>
 		    <div class="carousel-item">
-		      <img src="images/membres/3.jpg" class="img-thumbnail" alt="..." width="250px">
+		      <img src="images/membres/6.jpg" class="img-thumbnail" alt="..." width="250px">
 		      <h5 class="mt-5">Mamadou Hassimou Diallo</h5>
 		      <p>Étudiant</p>
 		      <p>Email : <a href="mailto:mamadouhassimou.diallo@unchk.edu.sn">mamadouhassimou.diallo@unchk.edu.sn</a></p>
 		    </div>
 		    <div class="carousel-item">
-		      <img src="images/membres/3.jpg" class="img-thumbnail" alt="..." width="250px">
+		      <img src="images/membres/7.jpg" class="img-thumbnail" alt="..." width="250px">
 		      <h5 class="mt-5">Momar Coumba Kane</h5>
 		      <p>Étudiant</p>
 		      <p>Email : <a href="mailto:momarcoumba.kane@unchk.edu.sn">momarcoumba.kane@unchk.edu.sn</a></p>
@@ -112,7 +112,7 @@
 		      <p>Email : <a href="mailto:mameadama.bousso@unchk.edu.sn">mameadama.bousso@unchk.edu.sn</a></p>
 		    </div>
 		    <div class="carousel-item">
-		      <img src="images/membres/3.jpg" class="img-thumbnail" alt="..." width="250px">
+		      <img src="images/membres/5.jpg" class="img-thumbnail" alt="..." width="250px">
 		      <h5 class="mt-5">Khadidiatou Mbathie</h5>
 		      <p>Étudiante</p>
 		      <p>Email : <a href="mailto:khadidiatou.mbathie@unchk.edu.sn">khadidiatou.mbathie@unchk.edu.sn</a></p>
