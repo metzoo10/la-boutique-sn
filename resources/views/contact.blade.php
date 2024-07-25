5a1fa97c3731873467aeaeb2af0d47fedb9b0df3
Ahmed-loum <ahmedloum41@gmail.com>
2024-07-25 00:17:41 +0000

    page contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,15 +3,49 @@
 @section('content')
 	<!-- Page de contact -->
 
-	<div class="container mt-5">
-		<nav aria-label="breadcrumb">
-	  		<ol class="breadcrumb">
-	    		<li class="breadcrumb-item"><a class="text-uppercase text-info text-decoration-none" href="/">Accueil</a></li>
-	    		<li class="breadcrumb-item active text-uppercase" aria-current="page">Contact</li>
-	  		</ol>
-		</nav>
-	</div>
-
-	<h1>Nos produits arrivent bientôt !</h1>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, fuga.</p>
-@endsection
+	
+	<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> contact </title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+		          <nav aria-label="breadcrumb">
+	  		        <ol class="breadcrumb">
+	    		        <li class="breadcrumb-item"><a class="text-uppercase text-info text-decoration-none" href="/">Accueil</a></li>
+	    		        <li class="breadcrumb-item active text-uppercase" aria-current="page">Contact</li>
+	  		        </ol>
+		          </nav>
+	            </div>
+               </div>
+                <div class="col-sm-12 col-lg-6">
+                    <div class="right">
+                       <i class="fa fa-caret-left"></i>
+                        <form>
+                          <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Your Name</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                          </div>
+                          <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                          </div>
+                          <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Message</label>
+                              <textarea type="password" class="form-control" id="exampleInputPassword1"></textarea>
+                          </div>
+                          <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
