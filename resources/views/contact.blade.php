d6183294f88c442a1c52df6f522721368fcb3f56
metzoo10 <mister_mef365@outlook.com>
2024-07-27 00:21:22 +0000

    Page contact : logique et table contacts ajoutés

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,12 @@
 @section('content')
 	<!-- Page de contact -->
 
+  @if(session('contact_success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('contact_success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  @endif
   <div class="container mt-5">
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
@@ -14,18 +20,19 @@
   <div class="container mt-5 mb-5 border border-secondary py-4">
     <div class="h5 pb-2 mt-2 mb-4 text-uppercase text-muted border-bottom border-secondary">Formulaire de contact</div>
     <div class="h6 pb-2 mt-2 mb-4 text-muted">Vous souhaitez nous contacter ? Remplissez ce formulaire et laissez-nous votre message !</div>
-    <form>
+    <form action="{{ route('contact.update') }}" method="POST">
+        @csrf
         <div class="form-floating mb-3">
-            <input class="form-control" placeholder="Entrer votre nom et prénom" id="floatingInput">
-            <label for="floatingInput">Prénom & Nom</label>
+            <input type="text" class="form-control" placeholder="Entrer votre nom et prénom" id="floatingInput" id="name" name="name" required>
+            <label for="floatingInput" for="name">Prénom & Nom</label>
         </div>
         <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Adresse mail</label>
+            <input type="email" class="form-control" id="floatingInput" id="email" name="email" placeholder="name@example.com" required>
+            <label for="floatingInput" for="email">Adresse mail</label>
         </div>
         <div class="form-floating mb-3">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-            <label for="floatingTextarea">Message</label>
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" id="message" name="message" rows="4" required></textarea>
+            <label for="floatingTextarea" for="message">Message</label>
         </div>
         <button type="submit" class="btn btn-info mt-4 mb-3">Envoyer</button>
     </form>
