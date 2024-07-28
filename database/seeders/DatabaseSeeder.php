5d0003bc25577dec6e6aee9c8dc432ac00be31b6
mor <tallpape32@gmail.com>
2024-07-28 15:05:27 +0000

    AJOUT DE UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,9 @@ class DatabaseSeeder extends Seeder
         $this->call(CategoriesTableSeeder::class);
 
         $this->call(ProduitsTableSeeder::class);
+
+        $this->call(UserSeeder::class);
+
         /*
         User::factory()->create([
             'name' => 'Test User',
