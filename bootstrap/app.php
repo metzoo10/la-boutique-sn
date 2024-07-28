bbe3fed0f300475e4635b00348305d4f83cea856
mor <tallpape32@gmail.com>
2024-07-28 15:00:49 +0000

    AJOUT DES ALIAS DES MIDDLEWARES CREE

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\Client;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,6 +14,13 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectGuestsTo(fn () => route('auth.Formlogin'));
+        $middleware->alias([
+            'admin' => Admin::class,
+
+            'client' => Client::class,
+
+
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
